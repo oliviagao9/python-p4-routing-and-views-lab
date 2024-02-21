2dc9413f1b76b3c184a65a4d6189fbc7493288bd
oliviagao9 <oliviagao9@gmail.com>
2024-02-20 19:54:11 -0600

    Done

--- server/app.py
+++ server/app.py
@@ -4,5 +4,46 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+# A print_string view should take one parameter, a string. 
+# It should print the string in the terminal and display 
+# it in the web browser. Its URL should be of the format /print/parameter.
+@app.route('/print/<string:route>')
+def print_string(route):
+    print(route)
+    return route
+
+# A count() view should take one parameter, an integer. 
+# It should display all numbers in the range of that parameter on separate lines. 
+# Its URL should be of the format /count/parameter.
+@app.route('/count/<int:number>')
+def count(number):
+    count = f''
+    for n in range (number):
+        count += f'{n}\n'
+    return count
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+
+    elif operation == '-':
+        return str(num1 - num2)
+
+    elif operation == "*":
+        return str(num1 * num2)
+
+    elif operation == 'div':
+        return str(num1 / num2)
+
+    elif operation == '%':
+        return str (num1 % num2)
+
+    return 'opeartion is not recongized. Please use one of the following: + - * div %'
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
